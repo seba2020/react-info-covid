89194fab33e4e47f6374bf8320d914611b55ffb4
Sebastian Hondarza <sebastian.hondarza@gmail.com>
2020-11-05 00:26:59 -0300

    implementacion de select con continentes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,37 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardItem from './CardItem';
 
 const Main = () => {
 
     const [globalData, setGlobalData] = useState([]);
-    const [isLoading, setIsloading] = useState(true);
+    const [continentData, setContinentData] = useState([]);
+    const [isLoading, setIsloading] = useState(false);
+    const [continents, setContinents] = useState([]);
+    const [selectContinent, setSelectContinent] = useState('00');
+    const [filterData, setFilterData] = useState([]);
 
     const currentDate = new Date();
     const month = currentDate.getMonth() + 1;
     const year = currentDate.getFullYear();
     const day = currentDate.getDate();
 
-    useState(() => {
-        fetch('https://disease.sh/v3/covid-19/all')
-            .then(res => res.json())
-            .then(response => {
-                console.log("Main -> response", response)
-                setGlobalData(response)
-
-
-                setTimeout(() => setIsloading(false), 3000);
-            })
-            .catch((err) => console.log('fallo la wea ', err));
+    useEffect(() => {
+        getGlobalData();
+        getContinentsData();
     }, []);
 
+    const getGlobalData = () => {
+        return fetch('https://disease.sh/v3/covid-19/all')
+            .then(res => res.json())
+            .then(response => {
+                setGlobalData(response)
+            })
+            .catch((err) => console.log('err get all info ', err));
+    }
+
+    const getContinents = (data) => {
+        setContinents([]);
+        const continentsMap = data.map(item => item.continent);
+        setContinents([...continents, ...continentsMap]);
+    }
+
+    const getContinentsData = () => {
+        return fetch('https://disease.sh/v3/covid-19/continents')
+            .then(res => res.json())
+            .then(response => {
+                setContinentData(response);
+                getContinents(response);
+            })
+            .catch(err => console.log('err get all continents ', err));
+    }
 
     const numberWithCommas = (x) => {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     }
 
-    const ListGlobalItem = Object.keys(globalData).map((item) =>
-        <CardItem key={item} className="card-item" title={item} count={numberWithCommas(globalData[item])} />
+    const Items = Object.keys(filterData).map((item) =>
+        <CardItem key={item} className="card-item" title={item} count={numberWithCommas(filterData[item])} />
     );
+
+    const optionContinent = continents.map((item) =>
+        <option key={Math.floor(Math.random() * 1000)}>{item}</option>
+    );
+
+    const handleSelectContinent = (event) => {
+        setIsloading(true);
+        setSelectContinent(event.target.value);
+        if (event.target.value === '0') {
+            fakeLazyLoading();
+            setFilterData(globalData);
+        } else {
+            const filteredData = continentData.find(item => item.continent === event.target.value);
+            fakeLazyLoading();
+            setFilterData(filteredData);
+        }
+    }
+
+    const fakeLazyLoading = () => {
+        setTimeout(() => {
+            setIsloading(false);
+        }, 1000);
+    }
 
 
     return (
@@ -42,10 +85,16 @@ const Main = () => {
                 <h6 className="subheader__subtitle">Developed with React</h6>
             </section>
             <span className="date__title">Current Date: {day}/{month}/{year}</span>
+
+            <select onChange={handleSelectContinent} value={selectContinent}>
+                <option value="00" disabled>--Selection--</option>
+                <option value="0">All Continents</option>
+                {optionContinent}
+            </select>
             {
                 isLoading
                     ? <div className="loader"></div>
-                    : <div className="main">{ListGlobalItem}</div>
+                    : <div className="main">{Items}</div>
             }
         </>
     )
